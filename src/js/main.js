048f95c66ac5ed87bb5362ccb8a697e9de25293e
Vladyslav_Shynder <shynderv@gmail.com>
2020-02-22 14:31:12 +0200

    burger menu

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+var burgerButton = document.getElementById("burger");
+var falloutMenu = document.getElementById("burger-menu");
+
+burgerButton.onclick = function() {
+    if (burgerButton.src.includes("images/nav__burger.png")) {
+        falloutMenu.style.opacity = "1";
+        falloutMenu.style.visibility = "visible";
+        burgerButton.src = "images/close.png";
+    } else if (burgerButton.src.includes("images/close.png")) {
+        falloutMenu.style.opacity = "0";
+        falloutMenu.style.visibility = "hidden";
+        burgerButton.src = "images/nav__burger.png";
+    }
+};
